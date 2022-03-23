a22347b8f069fe941608e11c6f631c3327859b32
Anusha <anushavalmiki.amaze@gmail.com>
2022-03-23 16:28:07 +0530

    aaaaa

--- src/app/Pages/trainee-report/trainee-report.component.ts
+++ src/app/Pages/trainee-report/trainee-report.component.ts
@@ -16,6 +16,7 @@ export class TraineeReportComponent implements OnInit {
 
 
   search:any;
+  count:any;
   constructor(private LearningService:LearningService,private ActivatedRoute:ActivatedRoute ) { }
 
   ngOnInit(): void {
@@ -31,6 +32,7 @@ export class TraineeReportComponent implements OnInit {
      this.LearningService.GetTrainerReport(0,0).subscribe(data=>{
        this.employeereportlist=data;
        this.dummemployeereportlist=data;
+       this.count=this.employeereportlist.length
      }
       )
    }
@@ -83,7 +85,13 @@ export class TraineeReportComponent implements OnInit {
   courseid:any;
   getcourseid(event:any){
     this.courseid=event.target.value;
-    this.employeereportlist=this.dummemployeereportlist.filter((x: { courseID: any; })=>x.courseID==this.courseid);
+    if (event.target.value!='0'){
+      this.employeereportlist=this.dummemployeereportlist.filter((x: { courseID: any; })=>x.courseID==this.courseid);
+    }
+    else{
+      this.GetTrainerReport();
+    }
+   
   }
  
 
