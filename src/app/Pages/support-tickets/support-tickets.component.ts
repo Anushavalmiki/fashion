4969e2c90ed7010a4dc48a4eefc252167b31cd5f
Anusha <anushavalmiki.amaze@gmail.com>
2022-03-24 11:02:16 +0530

    aaaaa

--- src/app/Pages/support-tickets/support-tickets.component.ts
+++ src/app/Pages/support-tickets/support-tickets.component.ts
@@ -12,4 +12,23 @@ export class SupportTicketsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  
+  files: File[] = [];
+  onSelect(event: { addedFiles: any; }) {
+    debugger
+    console.log(event);
+    this.files.push(event.addedFiles[0]);
+    // this.uploadattachments();
+    console.log("content", this.files);
+  }
+
+
+  onRemove(event: any) {
+    debugger
+    console.log(event);
+    this.files.splice(this.files.indexOf(event), 1);
+  }
+
+
 }
